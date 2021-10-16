9d26184368d8880fe5f5caad79b07e49c9b0cdb5
haduong <ha.duongquang1908@gmail.com>
2021-10-16 23:03:42 +0700

    add new

---
+++ GameDetecPose.py
@@ -0,0 +1,28 @@
+import cv2
+import mediapipe as mp
+import time
+import poseDetector as poseDt
+
+def main():
+
+    DIR = 'C:/Users/duong/Documents/Python/Video/'
+    file = '2.mp4'
+    cap = cv2.VideoCapture(DIR + file)
+    detector = poseDt.poseDetector()
+
+    pTime = 0
+    while True:
+        success, img = cap.read()
+        img = detector.findPose(img)
+        detector.findPosition(img)
+
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+        cv2.putText(img, str(int(fps)), (70, 50), cv2.FONT_HERSHEY_PLAIN, 3,
+                    (255, 0, 0), 3)
+        cv2.imshow("Image", img)
+        cv2.waitKey(1)
+
+if __name__ == "__main__":
+    main()
---
+++ poseDetector.py
@@ -0,0 +1,107 @@
+import cv2
+import time
+import mediapipe as mp
+import enum
+import math
+class poseDetector():
+    def __init__(self, mode = False,modelComplex =1,smoothlm = True,
+                 enableSeg = False, smoothSeg = True, detectionCon = 0.5,trackingCon = 0.5):
+        self.mode = mode
+        self.modelComplex = modelComplex
+        self.smoothlm = smoothlm
+        self.enableSeg = enableSeg
+        self.smoothSeg = smoothSeg
+        self.detectionCon = detectionCon
+        self.trackingCon = trackingCon
+        self.mpPose = mp.solutions.pose
+        self.pose = self.mpPose.Pose(self.mode,self.modelComplex,self.smoothlm,
+                 self.enableSeg, self.smoothSeg, self.detectionCon,self.trackingCon)
+        self.mpDraw = mp.solutions.drawing_utils
+
+    def findPose(self, img, draw=True):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.pose.process(imgRGB)
+        if self.results.pose_landmarks:
+            if draw:
+                self.mpDraw.draw_landmarks(img, self.results.pose_landmarks,
+                                           self.mpPose.POSE_CONNECTIONS)
+        return img
+
+    def findPosition(self, img, draw=True):
+        self.lmList = []
+        if self.results.pose_landmarks:
+            for id, lm in enumerate(self.results.pose_landmarks.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                self.lmList.append([id, cx, cy])
+                if draw:
+                    cv2.circle(img, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
+                    print("id: ", id )
+                    print("cx: ", cx )
+                    print("cy: ", cy )
+        return self.lmList
+
+    def findAngle(self, img, p1, p2, p3, draw=True):
+        # Get the landmarks
+        x1, y1 = self.lmList[p1][1:]
+        x2, y2 = self.lmList[p2][1:]
+        x3, y3 = self.lmList[p3][1:]
+        # Calculate the Angle
+        angle = math.degrees(math.atan2(y3 - y2, x3 - x2) -
+                             math.atan2(y1 - y2, x1 - x2))
+        #if angle & lt; 0:
+        #   angle += 360
+        # print(angle)
+        # Draw
+        if draw:
+            cv2.line(img, (x1, y1), (x2, y2), (255, 255, 255), 3)
+            cv2.line(img, (x3, y3), (x2, y2), (255, 255, 255), 3)
+            cv2.circle(img, (x1, y1), 10, (0, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x1, y1), 15, (0, 0, 255), 2)
+            cv2.circle(img, (x2, y2), 10, (0, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x2, y2), 15, (0, 0, 255), 2)
+            cv2.circle(img, (x3, y3), 10, (0, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x3, y3), 15, (0, 0, 255), 2)
+            cv2.putText(img, str(int(angle)), (x2 - 50, y2 + 50),
+                        cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
+        return angle
+    def set_modelComplex(self,modelComplex):
+        self.modelComplex = modelComplex
+
+class Pose_Landmark(enum.IntEnum):
+  """The 33 pose landmarks."""
+  NOSE = 0
+  LEFT_EYE_INNER = 1
+  LEFT_EYE = 2
+  LEFT_EYE_OUTER = 3
+  RIGHT_EYE_INNER = 4
+  RIGHT_EYE = 5
+  RIGHT_EYE_OUTER = 6
+  LEFT_EAR = 7
+  RIGHT_EAR = 8
+  MOUTH_LEFT = 9
+  MOUTH_RIGHT = 10
+  LEFT_SHOULDER = 11
+  RIGHT_SHOULDER = 12
+  LEFT_ELBOW = 13
+  RIGHT_ELBOW = 14
+  LEFT_WRIST = 15
+  RIGHT_WRIST = 16
+  LEFT_PINKY = 17
+  RIGHT_PINKY = 18
+  LEFT_INDEX = 19
+  RIGHT_INDEX = 20
+  LEFT_THUMB = 21
+  RIGHT_THUMB = 22
+  LEFT_HIP = 23
+  RIGHT_HIP = 24
+  LEFT_KNEE = 25
+  RIGHT_KNEE = 26
+  LEFT_ANKLE = 27
+  RIGHT_ANKLE = 28
+  LEFT_HEEL = 29
+  RIGHT_HEEL = 30
+  LEFT_FOOT_INDEX = 31
+  RIGHT_FOOT_INDEX = 32
+
+
